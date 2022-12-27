2f4140cdc76aa8c0648d7f314861b62f6bf51bf1
sharlene <sharlenebon6@gmail.com>
2022-12-27 23:18:20 +0800

    event acknowledgements

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const path = require('path')
 const http = require('http')
 const express = require ('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
+
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -20,11 +22,24 @@ io.on ('connection', (socket) => {
     socket.emit('message', 'Welcome!')
     socket.broadcast.emit('message', 'A new has joined!')
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if(filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
         io.emit('message', message)
+        callback()
     })
-socket.on('disconnect', () => {
-    io.emit('message', 'A user has left!')
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('message', (coords))
+        callback()
+    })  
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
 })
 
 })
