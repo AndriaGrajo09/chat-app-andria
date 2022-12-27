30dea3f189b13f74ba386072a09d388c945b9a1c
Andria <andriagrajo@sorsu.edu.ph>
2022-12-27 20:53:04 +0800

    edited socket.io events

--- src/index.js
+++ src/index.js
@@ -19,6 +19,12 @@ io.on ('connection', (socket) => {
 
     socket.emit('countUpdated', count)
 
+    socket.on('increment', () => {
+        count++
+        //socket.emit('countUpdated', count)
+        io.emit('countUpdated', count)
+    })
+
 
 })
 
