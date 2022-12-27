577679e4ecf324d56e18aa8727fa3e5b545ab6ed
andria <andriagrajo.sorsu.edu.ph>
2022-12-28 01:13:58 +0800

    working with time

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require ('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const {generateMessage}= require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -19,8 +20,8 @@ app.use(express.static(publicDirectoryPath))
 io.on ('connection', (socket) => {
     console.log('New web socket connection!')
 
-    socket.emit('message', 'Welcome!')
-    socket.broadcast.emit('message', 'A new has joined!')
+    socket.emit('message', generateMessage('Welcome!'))
+    socket.broadcast.emit('message', 'A new user has joined!')
 
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
