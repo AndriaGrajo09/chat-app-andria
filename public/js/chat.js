36929687929538a65fc720916bdb6b3b9c4f719b
sharlene <sharlenebon6@gmail.com>
2022-12-27 21:54:56 +0800

    edited socket.io events

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,6 +4,15 @@ socket.on('message', (message) => {
     console.log(message)
 })
 
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = document.querySelector('input').value
+
+    socket.emit('sendMessage', message)
+
+})
+
 //socket.on('countUpdated', (count) => {
  //   console.log('The count has been updated.', count)
 //})
